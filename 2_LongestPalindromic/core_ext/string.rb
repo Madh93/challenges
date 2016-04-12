b8e53b5c5fb9e3b5d4d054739611bfe512f8bfdd
Madh93 <michelangelo93@gmail.com>
2016-04-13 00:28:14 +0100

    return more than one string

--- 2_LongestPalindromic/core_ext/string.rb
+++ 2_LongestPalindromic/core_ext/string.rb
@@ -1,19 +1,18 @@
 class String
   def longest_palindromic
-    longest, tmp = "",""
+    longest, tmp = [],""
+    max_size = 0
     reverse = self.reverse
 
     self.each_char.with_index(0) do |c,i|
-      if c == reverse[i]
-        tmp << c
-      else
-        tmp = ""
-      end
-      if tmp.size > longest.size
-        longest = tmp 
-      end
+      # Compare char
+      tmp = (c == reverse[i]) ? tmp << c : ""
+      # Find longest string size
+      max_size = (longest.empty?) ? 0 : longest.max_by(&:size).size
+      # Add in array
+      longest << tmp if tmp.size >= max_size
     end
-  
-    longest
+    # Remove duplicates and shortest elements
+    longest.uniq.delete_if { |s| s.size < max_size }
   end
 end
